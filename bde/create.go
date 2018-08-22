264b6e013b1ff80b3699a66be6e9145f1921dd8e
Brett Demetris <brett.demetris@airbnb.com>
2018-08-22 16:15:02 -0700

    fix logic for enabling the tpm

--- bde/create.go
+++ bde/create.go
@@ -1,20 +1,77 @@
 package bde
 
 import (
+	"fmt"
 	"os/exec"
+	"regexp"
 
 	"github.com/pkg/errors"
 )
 
-//CreateKeyProtector generates another key of type RecoveryPassword
+//CreateRecoveryPasswordProtector generates another key of type RecoveryPassword
 //Windows seems to allow infinite keys to exist, each time this runs it will create a new key
 //if the disk is encrypted already.  If the disk is not encrypted it will encrypt it.
-func CreateKeyProtector() error {
-	cmd := exec.Command("powershell", "Enable-Bitlocker", "-MountPoint", "$env:SystemDrive", "-EncryptionMethod", "XtsAes128", "-RecoveryPasswordProtector", "-UsedSpaceOnly")
+func CreateRecoveryPasswordProtector() (string, error) {
+	cmd := exec.Command("C:\\Windows\\System32\\manage-bde.exe", "-protectors", "-add", "-rp", "c:")
+
+	o, err := cmd.Output()
+	if err != nil {
+		return "", errors.Wrap(err, "exec Enable-Bitlocker: recovery password")
+	}
+
+	r, _ := regexp.Compile("{[A-H0-9-]{36}}")
+
+	id := r.FindString(string(o))
+
+	return id, nil
+}
+
+//CreateTpmProtector enables the TPM on a device
+func CreateTpmProtector() error {
+	cmd := exec.Command("C:\\Windows\\System32\\manage-bde.exe", "-protectors", "-add", "-tpm", "c:")
 
 	_, err := cmd.Output()
 	if err != nil {
-		return errors.Wrap(err, "exec Enable-Bitlocker")
+		return errors.Wrap(err, "exec Enable-Bitlocker: tpm protector")
+	}
+
+	return nil
+}
+
+//CreateProtectorsIfMissing is meant to handle an out of the box situation
+func CreateProtectorsIfMissing() error {
+	//First see if there is a key to rotate, if not, create one
+	key, err := GetActiveRecoveryPassword()
+	if err != nil {
+		return errors.Wrap(err, "create: cant get active recovery password")
+	}
+
+	if key == "" {
+		fmt.Println("No Recovery Passwords, Creating One...")
+		_, err := CreateRecoveryPasswordProtector()
+		if err != nil {
+			return errors.Wrap(err, "create: creating initial key protector")
+		}
+	}
+
+	//Get the status to see if we have a TPM or better
+	status, err := GetEncryptionStatus()
+	if err != nil {
+		return errors.Wrap(err, "create: getting encryption status")
+	}
+
+	for _, element := range status.KeyProtector {
+		t := 3
+		if element.KeyProtectorType != t {
+			fmt.Println("TPM or Equivalent is Present")
+			return nil
+		}
+	}
+
+	fmt.Println("No TPM or Equivalent Found, Enabling the TPM Protector")
+	err = CreateTpmProtector()
+	if err != nil {
+		return errors.Wrap(err, "create: enabling the TPM")
 	}
 
 	return nil
--- bde/rotate.go
+++ bde/rotate.go
@@ -10,21 +10,49 @@ import (
 //It will then generate a new key
 func RotateKey() error {
 
+	err := CreateProtectorsIfMissing()
+	if err != nil {
+		return errors.Wrap(err, "rotate: create key protectors if missing")
+	}
+
+	//First see if there is a key to rotate, if not, create one
+	// key, err := GetActiveRecoveryPassword()
+	// if err != nil {
+	// 	return errors.Wrap(err, "rotate: cant get active recovery password")
+	// }
+
+	// if key == "" {
+	// 	fmt.Println("No Recovery Passwords, Creating One...")
+	// 	_, err := CreateRecoveryPasswordProtector()
+	// 	if err != nil {
+	// 		return errors.Wrap(err, "rotate: creating initial key protector")
+	// 	}
+	// 	return nil
+	// }
+
+	//If we have a key, rotate it
 	status, err := GetEncryptionStatus()
 	if err != nil {
 		return errors.Wrap(err, "rotate: getting encryption status")
 	}
 
+	//This is here to protect us from unknown states, but we return early above so we should never get here
 	if len(status.KeyProtector) == 0 {
 		fmt.Println("no existing key protectors, need to create one")
-		err := CreateKeyProtector()
+		_, err := CreateRecoveryPasswordProtector()
 		if err != nil {
 			return errors.Wrap(err, "rotate: creating initial key protector")
 		}
 		return nil
 	}
 
-	fmt.Println("active keys found, rotating")
+	fmt.Println("Active Key Found, Rotating...")
+
+	k, err := CreateRecoveryPasswordProtector()
+	if err != nil {
+		return errors.Wrap(err, "rotate: creating another key protector")
+	}
+
 	var a []string
 
 	//KeyProtector with Type 3 is RecoveryPassword
@@ -34,18 +62,40 @@ func RotateKey() error {
 		}
 	}
 
-	//Delete the first element in the ID slice
-	//Windows seems to respect all keys, but only tells the users the first item is active
-	//TODO This can error
-	err = DeleteKeyProtector(a[0])
-	if err != nil {
-		return errors.Wrap(err, "rotate: failed to delete old key")
+	//For debug
+	if len(a) == 0 {
+		return errors.New("rotate: no key found, but we expected one")
 	}
 
-	err = CreateKeyProtector()
-	if err != nil {
-		return errors.Wrap(err, "rotate: creating another key protector")
+	//Delete the recovery passwords on disk that we didn't just make
+	//Windows seems to respect all keys, but only tells the users the first item is active
+	//TODO This can error
+	for _, element := range a {
+		if element != k {
+			err = DeleteKeyProtector(element)
+		}
+		if err != nil {
+			return errors.Wrap(err, "rotate: failed to delete old key")
+		}
 	}
+
+	//Finally, make sure we have a TPM protector or something equivalent
+	//This tests to see if all key protectors are of type 3 (recovery password)
+	//If all are of type 3 we assume the TPM (or something) is not active, and needs to be
+	//TODO this entire function does to much, break it up!
+	// for _, element := range status.KeyProtector {
+	// 	t := 3
+	// 	if element.KeyProtectorType != t {
+	// 		fmt.Println("TPM or equivalent is present")
+	// 		return nil
+	// 	}
+	// }
+
+	// fmt.Println("No TPM or equivalent found, enabling the TPM protector")
+	// err = CreateTpmProtector()
+	// if err != nil {
+	// 	return errors.Wrap(err, "roate: enabling the TPM")
+	// }
 
 	return nil
 }
